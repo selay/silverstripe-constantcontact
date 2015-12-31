36aecd6eb042fcb5fd4a5f116c58b5eb993d09a2
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:25:48 +1300

    Converted to PSR-2

--- code/SS_ConstantContact.php
+++ code/SS_ConstantContact.php
@@ -4,8 +4,9 @@
  * @author Elmin wwwelminmail@gmail.com
  * @copyright 2014
  */
-class SS_ConstantContactExtension extends DataExtension { 
-        private static $db = array(
+class SS_ConstantContactExtension extends DataExtension
+{
+    private static $db = array(
           'CcTitle'=>'Varchar(255)',
           'CcApiKey'=>'Varchar(255)',
           'CcAccessToken'=>'Varchar(255)',
@@ -20,98 +21,104 @@ class SS_ConstantContactExtension extends DataExtension {
           'CcInputFieldClasses'=>'Varchar(255)',
           'CcSubmitButtonText'=>'Varchar',
           'CcSubmitButtonClasses'=>'Varchar(255)',
-        );  
-        public function onAfterWrite(){
-
-              $listIDs=$this->owner->CcListID->getValues();
-              $lists=serialize(singleton('SS_ConstantContactController')->getLists(true,  $listIDs));
-              if ($lists!= $this->owner->CcListCache){
-                $this->owner->CcListCache=$lists;
-                $this->owner->write();
-              }
-           
+        );
+    public function onAfterWrite()
+    {
+        $listIDs=$this->owner->CcListID->getValues();
+        $lists=serialize(singleton('SS_ConstantContactController')->getLists(true,  $listIDs));
+        if ($lists!= $this->owner->CcListCache) {
+            $this->owner->CcListCache=$lists;
+            $this->owner->write();
         }
-        public function updateCMSFields(FieldList $fields) {
-              $aLists=singleton('SS_ConstantContactController')->getLists(true);
+    }
+    public function updateCMSFields(FieldList $fields)
+    {
+        $aLists=singleton('SS_ConstantContactController')->getLists(true);
 
-             $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcTitle')->setTitle('Widget Title')->setAttribute('placeholder', 'Widget Title'));
-            $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcApiKey')->setTitle('API KEY <span style="color:red">*</span>')->setAttribute('placeholder', 'API KEY'));
-            $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcAccessToken')->setTitle('ACCESS TOKEN<span style="color:red">*</span>')->setAttribute('placeholder', 'ACCESS TOKEN'));
-            $contactList = ($this->owner->CcApiKey && $this->owner->CcAccessToken)
+        $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcTitle')->setTitle('Widget Title')->setAttribute('placeholder', 'Widget Title'));
+        $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcApiKey')->setTitle('API KEY <span style="color:red">*</span>')->setAttribute('placeholder', 'API KEY'));
+        $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcAccessToken')->setTitle('ACCESS TOKEN<span style="color:red">*</span>')->setAttribute('placeholder', 'ACCESS TOKEN'));
+        $contactList = ($this->owner->CcApiKey && $this->owner->CcAccessToken)
                 ? new MultiValueDropdownField('CcListID', 'Subscription List(s)', $aLists)
                 : LiteralField::create('CcListNote', '<strong>You can select your subscriber lists(s) after saving your API KEY and ACCESS TOKEN</strong>');
-            $fields->addFieldToTab("Root.ConstantContact", $contactList);
+        $fields->addFieldToTab("Root.ConstantContact", $contactList);
 
-            $fields->addFieldToTab("Root.ConstantContact", CheckboxField::create('CcDisplayZip')->setTitle('Show Postcode field'));
+        $fields->addFieldToTab("Root.ConstantContact", CheckboxField::create('CcDisplayZip')->setTitle('Show Postcode field'));
        
-            $fields->addFieldToTab("Root.ConstantContact", TextareaField::create('CcInfoText')->setTitle('Message under Title')->setAttribute('placeholder', 'This text will be displayed under Title in the widget.'));
-             $fields->addFieldToTab("Root.ConstantContact", TextareaField::create('CcRequiredMessage')->setTitle('Required Fields Error')->setAttribute('placeholder', 'Please write a message that will be displayed if required fields are not filled.'));
+        $fields->addFieldToTab("Root.ConstantContact", TextareaField::create('CcInfoText')->setTitle('Message under Title')->setAttribute('placeholder', 'This text will be displayed under Title in the widget.'));
+        $fields->addFieldToTab("Root.ConstantContact", TextareaField::create('CcRequiredMessage')->setTitle('Required Fields Error')->setAttribute('placeholder', 'Please write a message that will be displayed if required fields are not filled.'));
 
-            $fields->addFieldToTab("Root.ConstantContact", TextareaField::create('CcErrorMessage')->setTitle('Error Message')->setAttribute('placeholder', 'Please write an error message to be displayed if a contact can not be added.'));
-             $fields->addFieldToTab("Root.ConstantContact", TextareaField::create('CcAddedMessage')->setTitle('Successfully Created Message')->setAttribute('placeholder', 'Please write a success message to be displayed if a contact is successfully added to the list.'));
-              $fields->addFieldToTab("Root.ConstantContact", TextareaField::create('CcUpdatedMessage')->setTitle('Successfully Updated Message')->setAttribute('placeholder', 'Please write a success message to be displayed if a contact, who already exists in the list, is successfully updated.'));
+        $fields->addFieldToTab("Root.ConstantContact", TextareaField::create('CcErrorMessage')->setTitle('Error Message')->setAttribute('placeholder', 'Please write an error message to be displayed if a contact can not be added.'));
+        $fields->addFieldToTab("Root.ConstantContact", TextareaField::create('CcAddedMessage')->setTitle('Successfully Created Message')->setAttribute('placeholder', 'Please write a success message to be displayed if a contact is successfully added to the list.'));
+        $fields->addFieldToTab("Root.ConstantContact", TextareaField::create('CcUpdatedMessage')->setTitle('Successfully Updated Message')->setAttribute('placeholder', 'Please write a success message to be displayed if a contact, who already exists in the list, is successfully updated.'));
              
-              $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcInputFieldClasses')->setTitle('Input Field Classes')->setAttribute('placeholder', 'Please write classes you want to be added to the input fields.'));
+        $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcInputFieldClasses')->setTitle('Input Field Classes')->setAttribute('placeholder', 'Please write classes you want to be added to the input fields.'));
             
-             $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcSubmitButtonText')->setTitle('Submit button text')->setAttribute('placeholder', 'Please write a submit button text. e.g Sign up'));
+        $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcSubmitButtonText')->setTitle('Submit button text')->setAttribute('placeholder', 'Please write a submit button text. e.g Sign up'));
 
-              $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcSubmitButtonClasses')->setTitle('Submit button classes')->setAttribute('placeholder', 'Please write classes you want for the submit button'));
+        $fields->addFieldToTab("Root.ConstantContact", TextField::create('CcSubmitButtonClasses')->setTitle('Submit button classes')->setAttribute('placeholder', 'Please write classes you want for the submit button'));
 
-             $fields->addFieldToTab("Root.ConstantContact", LiteralField::create('LiteralL', 'Please use instructions <a target="_blank" href="https://developer.constantcontact.com/api-keys.html">here </a> to get API and Token. 
+        $fields->addFieldToTab("Root.ConstantContact", LiteralField::create('LiteralL', 'Please use instructions <a target="_blank" href="https://developer.constantcontact.com/api-keys.html">here </a> to get API and Token. 
                <div> If you select only one list above, the lists checkbox will not be shown in the front-end. If more than one, they will be displayed as checkboxes. If left empty, all available lists will be listed as checkboxes.</div> <div style="color:red">* All fields above are REQUIRED</div>'));
+    }
+    public static function hasMethod()
+    {
+    }
+    public static function Link()
+    {
+    }
 
-        }
-     public static function hasMethod(){}
-     public static function Link(){}
 
-
-     public function SS_ConstantContactForm(){
-
-        Requirements::css( CONSTANT_CONTACT_BASE . '/css/cc_ccs.css');
-        Requirements::javascript( CONSTANT_CONTACT_BASE . '/js/cc_js.js');
+    public function SS_ConstantContactForm()
+    {
+        Requirements::css(CONSTANT_CONTACT_BASE . '/css/cc_ccs.css');
+        Requirements::javascript(CONSTANT_CONTACT_BASE . '/js/cc_js.js');
         $config=$this->owner;
 
         $listIDs=$config->CcListID->getValues();
       
-        if (is_array( $listIDs)){
-        if (count( $listIDs)==1)
-           $listfield=HiddenField::create('list')->setValue(reset( $listIDs));
-        else {
-               $lists=$this->owner->CcListCache;
-               if (!$lists){ 
-                   $lists=singleton('SS_ConstantContactController')->getLists(true,  $listIDs);
-                   if ($lists) {
-                    $this->owner->CcListCache=serialize($lists);
-                    $this->owner->write();
-                  }
-               } else $lists=unserialize($lists);
+        if (is_array($listIDs)) {
+            if (count($listIDs)==1) {
+                $listfield=HiddenField::create('list')->setValue(reset($listIDs));
+            } else {
+                $lists=$this->owner->CcListCache;
+                if (!$lists) {
+                    $lists=singleton('SS_ConstantContactController')->getLists(true,  $listIDs);
+                    if ($lists) {
+                        $this->owner->CcListCache=serialize($lists);
+                        $this->owner->write();
+                    }
+                } else {
+                    $lists=unserialize($lists);
+                }
 
-               $listfield=CheckboxSetField::create('list', 'List Options', $lists)->addExtraClass('form-control '.$config->CcInputFieldClasses);
+                $listfield=CheckboxSetField::create('list', 'List Options', $lists)->addExtraClass('form-control '.$config->CcInputFieldClasses);
+            }
+        } else {
+            return false;
         }
-      } else return false; 
           
-          $fields = new FieldList(array(
+        $fields = new FieldList(array(
                  TextField::create('email')->setTitle('')->addExtraClass('form-control '.$config->CcInputFieldClasses)->setAttribute('placeholder', 'Email'),
                  TextField::create('first_name')->setTitle('')->addExtraClass('form-control '.$config->CcInputFieldClasses)->setAttribute('placeholder', 'First Name'),
                  TextField::create('last_name')->setTitle('')->addExtraClass('form-control '.$config->CcInputFieldClasses)->setAttribute('placeholder', 'Last Name'),
                  $listfield,
-                LiteralField::create('SignUpButton', '<input type="button" class="'.$config->CcSubmitButtonClasses.'" id="subscribe_to_cc_list" value="'.($config->CcSubmitButtonText?$config->CcSubmitButtonText:"Sign Up").'" />')  
+                LiteralField::create('SignUpButton', '<input type="button" class="'.$config->CcSubmitButtonClasses.'" id="subscribe_to_cc_list" value="'.($config->CcSubmitButtonText?$config->CcSubmitButtonText:"Sign Up").'" />')
             ));
 
-          if ($config->CcDisplayZip)  //if postcode field is needed. 
+        if ($config->CcDisplayZip) {  //if postcode field is needed. 
             $fields->push(TextField::create('postcode')->setTitle('')->addExtraClass('form-control '.$config->CcInputFieldClasses)->setAttribute('placeholder', 'Postcode'));
+        }
             
 
             // Create actions
             $actions = new FieldList(
                  FormAction::create('signup')->addExtraClass('small button')
-            );        
-            $form = new Form($this, 'SS_ConstantContactSignup', $fields, $actions);
-            $form->addExtraClass('custom constant-contect-signup');
+            );
+        $form = new Form($this, 'SS_ConstantContactSignup', $fields, $actions);
+        $form->addExtraClass('custom constant-contect-signup');
             // Load the form with previously sent data
             $form->loadDataFrom(Controller::curr()->getRequest()->postVars());
-            return $form;       
+        return $form;
     }
-
 }
-
--- code/SS_ConstantContactController.php
+++ code/SS_ConstantContactController.php
@@ -11,59 +11,66 @@ use Ctct\Components\Contacts\ContactList;
 use Ctct\Components\Contacts\EmailAddress;
 use Ctct\Exceptions\CtctException;
 
-class  SS_ConstantContactController extends ContentController{
-    static $allowed_actions = array('subscribe');
+class  SS_ConstantContactController extends ContentController
+{
+    public static $allowed_actions = array('subscribe');
     
     public static $url_handlers = array();
 
-    public function init() {
+    public function init()
+    {
         parent::init();
-        
-        
     }
 
- public function getLists($array=false, $only=false){
-     $config=SiteConfig::current_site_config();
-     $cc = new ConstantContact($config->CcApiKey);
-       try{
-        $lists=$cc->getLists($config->CcAccessToken);
-        if ($array){
-            $array=array();
-              foreach ($lists as $list) 
-                if ($only){
-                    if (in_array($list->id,$only))
-                   $array[$list->id]=$list->name;
+    public function getLists($array=false, $only=false)
+    {
+        $config=SiteConfig::current_site_config();
+        $cc = new ConstantContact($config->CcApiKey);
+        try {
+            $lists=$cc->getLists($config->CcAccessToken);
+            if ($array) {
+                $array=array();
+                foreach ($lists as $list) {
+                    if ($only) {
+                        if (in_array($list->id, $only)) {
+                            $array[$list->id]=$list->name;
+                        }
+                    } else {
+                        $array[$list->id]=$list->name;
+                    }
                 }
-               else $array[$list->id]=$list->name;
-             return $array;
-          }  
+                return $array;
+            }
             return $cc->getLists($config->CcAccessToken);
-        
+        } catch (CtctException $ex) {
+        }
+        return '';
+    }
 
-        } catch (CtctException $ex) {} 
-        return '';   
-     }
 
-
- public function subscribe(){
-         if (!$this->getRequest()->isAjax())  //if not ajax, say go and eat your grass. 
-            exit("Action is not allowed!");       
-         $return=array('action'=>'','message'=>'');
-         $config=SiteConfig::current_site_config();
-         $cc = new ConstantContact($config->CcApiKey);
-         $list=$this->getRequest()->postVar('list');
-         if ($config->CcDisplayZip)
-            $postcode=array('postal_code'=>$this->getRequest()->postVar('postcode')); 
-         else $postcode=0;
-         $email=$this->getRequest()->postVar('email');
-         $first_name=$this->getRequest()->postVar('first_name');
-         $last_name=$this->getRequest()->postVar('last_name');
+    public function subscribe()
+    {
+        if (!$this->getRequest()->isAjax()) {  //if not ajax, say go and eat your grass. 
+            exit("Action is not allowed!");
+        }
+        $return=array('action'=>'','message'=>'');
+        $config=SiteConfig::current_site_config();
+        $cc = new ConstantContact($config->CcApiKey);
+        $list=$this->getRequest()->postVar('list');
+        if ($config->CcDisplayZip) {
+            $postcode=array('postal_code'=>$this->getRequest()->postVar('postcode'));
+        } else {
+            $postcode=0;
+        }
+        $email=$this->getRequest()->postVar('email');
+        $first_name=$this->getRequest()->postVar('first_name');
+        $last_name=$this->getRequest()->postVar('last_name');
          
-        if (empty($email)||empty($first_name)||empty($last_name)||empty($list)){
+        if (empty($email)||empty($first_name)||empty($last_name)||empty($list)) {
             $return['action']='E';
             $return['message']=$config->CcRequiredMessage?$config->CcRequiredMessage:'Required fields are missing.';
-        }   
-          else try {
+        } else {
+            try {
         
         // check to see if a contact with the email addess already exists in the account
         $response = $cc->getContactByEmail($config->CcAccessToken, $email);
@@ -75,43 +82,54 @@ class  SS_ConstantContactController extends ContentController{
 
             $contact = new Contact();
             $contact->addEmail($email);
-              if (!is_array($list)) 
+            if (!is_array($list)) {
                 $contact->addList($list);
-            else 
-                foreach($list AS $l)
+            } else {
+                foreach ($list as $l) {
                     $contact->addList($l);
+                }
+            }
             $contact->first_name = $first_name;
             $contact->last_name = $last_name;
-            if ($postcode)
+            if ($postcode) {
                 $contact->addAddress($postcode);
-            $returnContact = $cc->addContact($config->CcAccessToken, $contact); 
-            if (!empty($returnContact)) $return['message']=$config->CcAddedMessage;
+            }
+            $returnContact = $cc->addContact($config->CcAccessToken, $contact);
+            if (!empty($returnContact)) {
+                $return['message']=$config->CcAddedMessage;
+            }
         // update the existing contact if address already existed
-        } else {            
+        } else {
             $return['action'] = "U";
 
             $contact = $response->results[0];
-            if (!is_array($list)) 
+            if (!is_array($list)) {
                 $contact->addList($list);
-            else 
-                foreach($list AS $l)
+            } else {
+                foreach ($list as $l) {
                     $contact->addList($l);
+                }
+            }
                 
             $contact->first_name = $first_name;
             $contact->last_name = $last_name;
-            if ($postcode)
+            if ($postcode) {
                 $contact->addAddress($postcode);
-            $returnContact = $cc->updateContact($config->CcAccessToken, $contact);  
-             if (!empty($returnContact)) $return['message']=$config->CcUpdatedMessage;
+            }
+            $returnContact = $cc->updateContact($config->CcAccessToken, $contact);
+            if (!empty($returnContact)) {
+                $return['message']=$config->CcUpdatedMessage;
+            }
         }
         
     // catch any exceptions thrown during the process and print the errors to screen
-    } catch (CtctException $ex) {
-        $ex=$ex->getErrors();
-        print_r($ex);
-        $return['action']='E';
-        $return['message']=$config->CcErrorMessage;
-    }
-    return json_encode($return);
+            } catch (CtctException $ex) {
+                $ex=$ex->getErrors();
+                print_r($ex);
+                $return['action']='E';
+                $return['message']=$config->CcErrorMessage;
+            }
+        }
+        return json_encode($return);
     }
 }
